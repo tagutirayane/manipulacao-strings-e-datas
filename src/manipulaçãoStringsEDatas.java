faa8ec83518bed592d0b9189c6c1f1b6c314a11e
Rayane Taguti <111579234+tagutirayane@users.noreply.github.com>
2022-09-11 18:16:51 -0300

    first commit

---
+++ src/manipulaçãoStringsEDatas.java
@@ -0,0 +1,37 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+
+public class manipulaçãoStringsEDatas {
+
+    public static void main(String[] args) {
+
+        // Olá, {nome}. Hoje é {dia-da-semana}. Bom dia.
+
+        String nome = "Rayane";
+
+        // ISO 8601
+        LocalDate hoje = LocalDate.now();
+        Locale brasil = new Locale("pt", "BR");
+        System.out.println(hoje.getDayOfWeek().getDisplayName(TextStyle.FULL, brasil));
+        String diaSemana = hoje.getDayOfWeek().getDisplayName(TextStyle.FULL, brasil);
+        String saudacao;
+        LocalDateTime agora = LocalDateTime.now();
+        if (agora.getHour() >= 0 && agora.getHour() < 12) {
+            saudacao = "bom dia";
+        } else if (agora.getHour() >= 12 && agora.getHour() < 18) {
+            saudacao = "boa tarde";
+        } else if (agora.getHour() >= 18 && agora.getHour() < 24) {
+            saudacao = "boa noite";
+        } else {
+            saudacao = "Olá.";
+        }
+
+        System.out.printf("Olá, %s. Hoje é %s, %s.%n", nome, diaSemana, saudacao.toLowerCase());
+
+
+
+    }
+}
